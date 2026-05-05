5ca248bdf2fe4222f5ca8d626c25234019a91fff
Kartikey Kumar Srivastava <kartikeyshrivastava@gmail.com>
2026-05-05 13:17:22 +0530

    Initial commit: Add hello_world and warm_temperatures programs

---
+++ programs/warm_temperatures.py
@@ -0,0 +1,22 @@
+def daily_temp(temps: list[int]) -> list[int]:
+    result = [0] * len(temps)
+    stack = []
+
+    # for i, temp in enumerate(temps):
+    #     while stack and temps[stack[-1]] < temp:
+    #         prev_index = stack.pop()
+    #         result[prev_index] = i - prev_index
+    #     stack.append(i)
+
+    for i in range(len(temps)):
+        while stack and stack[-1] < temps[i]:
+            prev_index = stack.pop()
+            result[prev_index] = i - prev_index
+            stack.pop()
+
+
+    return result
+        
+
+
+    
